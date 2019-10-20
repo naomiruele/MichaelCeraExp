1aa16df8e4f90b66a56b2cb321167a9aee6abf7a
Jesse Vogel <Jvogel@amplify.com>
2019-10-20 17:47:39 -0400

    added random velocity and boundary checks

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-let ceraX = 0
-let ceraY = 0
+let windowWidth = document.body.clientWidth
+let windowHeight = document.body.clientHeight
+const ceraWidth = 379
+const ceraHeight = 659
+let ceraX = windowWidth / 2
+let ceraY =  windowHeight / 2
+let vx = 1
+let vy = 1
+let ax = 2
+let ay = 2
 
-const image = new Image(379, 659)
+const image = new Image(ceraWidth, ceraHeight)
 const aspectRatio = image.height / image.width
 const scaledWidth = 100
 const scaledHeight = 100 * aspectRatio
-let windowWidth = document.body.clientWidth
-let windowHeight = document.body.clientHeight
+
+let canvas
 let context
 let interval
 
@@ -21,8 +29,25 @@ const init = () => {
 }
 
 const updateState = () => {
-    ceraX = Math.random() * windowWidth
-    ceraY = Math.random() * windowHeight
+    const sign = Math.random() > 0.5 ? -1 : 1
+    vx = vx + (sign * (Math.random() * ax))
+    vy = vy + (sign * (Math.random() * ay))
+    if (ceraX <= 0){
+        vx = 10
+    }
+    if (ceraY <= 0){
+        vy = 10
+    }
+    if (ceraX >= windowWidth - scaledWidth){
+        vx = -10
+    }
+    if (ceraY >= windowHeight - scaledHeight){
+        vy = -10
+    }
+
+    ceraX = ceraX + vx
+    ceraY = ceraY + vy
+    
 }
 
 const onTimerTick = () => {
@@ -31,12 +56,13 @@ const onTimerTick = () => {
 }
 
 const drawState = () => {
+    context.clearRect(0, 0, canvas.width, canvas.height);
     context.drawImage(image, ceraX, ceraY, scaledWidth, scaledHeight)
 }
 
 
 window.addEventListener('load', () => {
-    const canvas = document.getElementById('canvas')
+    canvas = document.getElementById('canvas')
     canvas.width = document.body.clientWidth
     canvas.height = document.body.clientHeight
     context = canvas.getContext('2d')
